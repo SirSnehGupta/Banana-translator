855bca36e89196a904fd07edcc31b23a64bb0bfd
SirSnehGupta <guptasneh302004@gmail.com>
2021-07-10 19:33:39 +0530

    Major changes

---
+++ app.js
@@ -0,0 +1,16 @@
+var click = document.querySelector('#translate')
+var input = document.querySelector('#input')
+var output = document.querySelector('#output')
+
+function urlConst(text){
+    return 'https://api.funtranslations.com/translate/minion.json?text='+text
+}
+function errorHandle(error){
+    console.log('Error occurred: '+error)
+    alert('Oops! Server Probs :(\nTry again after some time.')
+}
+function clickEventHandler(){
+    fetch(urlConst(input.value)).then(Response => Response.json()).then(json => console.log(json.contents.translated)).catch(errorHandle)
+    
+}
+click.addEventListener('click', clickEventHandler);
